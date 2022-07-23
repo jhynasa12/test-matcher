3fb3e78fdb660512b89949c58dec141dacdcce2c
Justin Hyland <justin@Justins-Mac-mini.local>
2022-07-23 14:43:43 -0400

    Initial Commit

---
+++ src/main/java/com/applause/test/matcher/testmatcher/TestMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.applause.test.matcher.testmatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/applause/test/matcher/testmatcher/TestMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applause.test.matcher.testmatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
